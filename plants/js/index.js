f1eefefc0292b1c01832763e37e46235200b857d
Uliana <uliana553663@gmial.com>
2023-01-31 08:29:05 +0300

    fix: hamburger desktop view

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,26 +1,51 @@
 window.onload = function(){
-console.log('Максимальное количество баллов: 75\n');
-console.log('Невыполненные пункты:');
-console.log('Не успела к дедлайну выполнить адаптивное меню');
+    console.log('Максимальное количество баллов: 100\n');
+    console.log('Невыполненные пункты:');
+    console.log('');
+    addTagsClickHandler();
+}
+
+const addTagsClickHandler=()=>{
+    let clickedButton = [];
+    document.querySelector('.service_btns').addEventListener('click',(e)=>{
+        if(e.target.classList.contains('btn')){
+            clickedButton.push(e.target);
+            if(clickedButton.length === 3){removeSelectedButtons()};
+            selectClickedButtons(clickedButton);
+        }
+    }) 
+}
+
+const removeSelectedButtons=() => {
+    let buttons = document.querySelectorAll('.btn_selected');
+    console.log(buttons)
+    buttons.forEach(btn=>{
+        console.log();
+        btn.classList.remove('btn_selected');
+        btn.classList.add('btn_unselected');
+    })
+}
+const selectClickedButtons=(clickedButton) => {
+    clickedButton.classList.remove('btn_unselected');
+    clickedButton.classList.add('btn_selected');
+
 }
 
 
 //Меню бургер открыть
 const iconMenu = document.querySelector('.hamburger');
 const menuBody = document.querySelector('.hamburger-nav');
-
 if (iconMenu) {
     iconMenu.addEventListener("click", function (e) {
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
     });
-
+}
 const menuLinks = document.querySelectorAll('.hamburger-nav-link');
     // Для каждого элемента меню при клике вызываем ф-ию
     menuLinks.forEach(menuLinks => {
       menuLinks.addEventListener("click", closeOnClick);
     });
-    
     // Закрытие попапа при клике на меню
     function closeOnClick() {
         iconMenu.classList.remove('_active');
@@ -31,6 +56,9 @@ const menuLinks = document.querySelectorAll('.hamburger-nav-link');
     //Закрытие меню при клике вне меню
     main.addEventListener('click', outsideEvtListener);
     footer.addEventListener('click', outsideEvtListener);
+  
+    
+
 
     function outsideEvtListener(evt) {
     if (evt.target === menuBody || menuBody.contains(evt.target)) {
@@ -40,4 +68,4 @@ const menuLinks = document.querySelectorAll('.hamburger-nav-link');
         iconMenu.classList.remove('_active');
         menuBody.classList.remove('_active');
     }
-}
+
