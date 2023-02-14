83184187296201ad10f843a0f81580fb84c29335
Uliana <uliana553663@gmial.com>
2023-02-14 10:53:46 +0300

    feat: add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,48 @@
+let randomNum = 0;
 window.onload = function(){
     showTime();
     getLocalStorage();
+    setBackgroundImage();
+    document.querySelector('.slide-next').addEventListener('click', getSlideNext);
+    document.querySelector('.slide-prev').addEventListener('click', getSlidePrev);
+}
+
+const getRandomNum=(min, max)=>{
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  randomNum = (Math.floor(Math.random() * (max - min + 1)) + min);
+  return
+}
+const getTimeOfDay=()=>{
+  const dayParts = ['morning', 'afternoon', 'evening','night'];
+  const date = new Date();
+  const hours = date.getHours();
+  return dayParts[Math.floor(hours/6)-1];
+};
+
+const setBackgroundImage=()=>{
+  const timeOfDay = getTimeOfDay();
+  console.log(randomNum)
+  if (randomNum === 0)
+    getRandomNum(1,20);
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${randomNum.toString().padStart(2,'0')}.jpg`
+  console.log(img.src);
+  img.onload = () => {   
+    const body = document.querySelector('body');
+    console.log('load!')
+    body.style.backgroundImage = `url('${img.src}')`;   
+    }
+  }
+
+const getSlideNext=()=>{
+  randomNum = randomNum === 20 ? 1 : randomNum + 1;
+  setBackgroundImage();
+}
+
+const getSlidePrev=()=>{
+  randomNum = randomNum === 1 ? 20 : randomNum - 1;
+  setBackgroundImage();
 }
 
 function setLocalStorage() {
@@ -11,10 +53,11 @@ window.addEventListener('beforeunload', setLocalStorage);
 
 function getLocalStorage() {
     if(localStorage.getItem('name')) {
-    const name = document.querySelector('.name');
-    name.value = localStorage.getItem('name');
+      const name = document.querySelector('.name');
+      name.value = localStorage.getItem('name');
     }
   };
+window.addEventListener('onload', getLocalStorage);
 
 const showTime=()=>{
     const options = {weekday: 'long', month: 'long', day: 'numeric'};
@@ -30,10 +73,8 @@ const showTime=()=>{
     const greeting_block = document.querySelector('.greeting');
     greeting_block.textContent=showGreeting();
 }
+
+
 const showGreeting=()=>{
-    const dayParts = ['morning', 'afternoon', 'evening','night'];
-    const date = new Date();
-    const hours = date.getHours();
-    const timeOfDay = dayParts[Math.floor(hours/6)];
-    return `Good ${timeOfDay}`;
+    return `Good ${getTimeOfDay()}`;
 };
